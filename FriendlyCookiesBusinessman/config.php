6ede5ea6dfd011bda09dbf15ddaa15a3ac22225b
Losses Don <losses.don@gmail.com>
2015-03-05 12:03:19 +0800

    引入FriendlyCookiesBusinessman插件基础代码

---
+++ FriendlyCookiesBusinessman/config.php
@@ -0,0 +1,27 @@
+<?php
+/********************************************************\
+ * FriendlyCookiesBusinessman plugin for Masochist-Board *
+ *                                 Ver 0.1 By Losses Don *
+ *                                  losses.don@gmail.com *
+ ********************************************************/
+
+$plugin_info = [
+    "IDENTIFICATION" => "losses.friendly.cookies.businessman"
+];
+
+$plugin_injector = [
+
+];
+
+$plugin_config = [
+    'site_key' => '',
+    'secret_key' => ''
+];
+
+$plugin_public_page = [
+    'cookie_shop' => [
+        'stylesheet' => 'style.css',
+        'script' => 'script.css',
+        'template' => 'shop.html'
+    ]
+];
